ec2bfc719c71d0b2b14ebf8f41a2a4d18bb86af3
Backgom <windsp7@coupang.com>
2016-01-05 20:35:05 +0900

    이벤트 예제 코드 작성

--- app/src/main/java/com/example/backgom/linearlayoutexample/MainActivity.java
+++ app/src/main/java/com/example/backgom/linearlayoutexample/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.backgom.linearlayoutexample;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	private TextView nameTextView;
+	private TextView ageTextView;
+	private TextView majorTextView;
+	private EditText nameEditText;
+	private EditText ageEditText;
+	private EditText majorEditText;
+
+	private FloatingActionButton fab;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +26,27 @@ public class MainActivity extends AppCompatActivity {
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		initializeViews();
+
+
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+
 			}
 		});
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
+	private void initializeViews() {
+		nameTextView = (TextView) findViewById(R.id.name_text);
+		ageTextView = (TextView) findViewById(R.id.age_text);
+		majorTextView = (TextView) findViewById(R.id.major_text);
+		nameEditText = (EditText) findViewById(R.id.name_edit);
+		ageEditText = (EditText) findViewById(R.id.age_edit);
+		majorEditText = (EditText) findViewById(R.id.major_edit);
+
+		fab = (FloatingActionButton) findViewById(R.id.fab);
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
 }
