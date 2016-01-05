df4e147b2ab80c98f6f4a7606725adfb5a4d1f52
Backgom <windsp7@coupang.com>
2016-01-05 21:58:47 +0900

    a

--- app/src/main/java/com/example/backgom/linearlayoutexample/MainActivity.java
+++ app/src/main/java/com/example/backgom/linearlayoutexample/MainActivity.java
@@ -7,12 +7,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 	private TextView nameTextView;
 	private TextView ageTextView;
 	private TextView majorTextView;
+	private TextView resulatTextView;
 	private EditText nameEditText;
 	private EditText ageEditText;
 	private EditText majorEditText;
@@ -28,14 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
 		initializeViews();
 
-
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-//asdjfjlsdfj
-				//asdjflkjsdf
+				String name = nameEditText.getText().toString();
+				String age = ageEditText.getText().toString();
+				String major = majorEditText.getText().toString();
 
-				//askdjflkjsdlf
+				if (validationCheck(name, age, major)) {
+					resulatTextView.setText(name + age + major);
+				}
 			}
 		});
 
@@ -45,11 +49,49 @@ public class MainActivity extends AppCompatActivity {
 		nameTextView = (TextView) findViewById(R.id.name_text);
 		ageTextView = (TextView) findViewById(R.id.age_text);
 		majorTextView = (TextView) findViewById(R.id.major_text);
+		resulatTextView = (TextView) findViewById(R.id.result_text);
 		nameEditText = (EditText) findViewById(R.id.name_edit);
 		ageEditText = (EditText) findViewById(R.id.age_edit);
 		majorEditText = (EditText) findViewById(R.id.major_edit);
 
 		fab = (FloatingActionButton) findViewById(R.id.fab);
+	}
+
+	private boolean validationCheck(String name, String age, String major) {
+
+		if (emptyString(name)) {
+			showToastPopup(nameTextView.getText().toString());
+			return false;
+		}
+
+		if (emptyString(age)) {
+			showToastPopup(ageTextView.getText().toString());
+			return false;
+		}
+
+		if (emptyString(major)) {
+			showToastPopup(majorTextView.getText().toString());
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean emptyString(String value) {
+		if (value == null) {
+			return true;
+		}
+
+		if ("".equals(value)) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	private void showToastPopup(String message) {
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 	}
 
 }
