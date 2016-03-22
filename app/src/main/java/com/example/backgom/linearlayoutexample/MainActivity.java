b08d090b04df3fba80e3af0218b552555307426e
Backgom <windsp7@coupang.com>
2016-03-22 14:40:31 +0900

    DialogSampleCode

--- app/src/main/java/com/example/backgom/linearlayoutexample/MainActivity.java
+++ app/src/main/java/com/example/backgom/linearlayoutexample/MainActivity.java
@@ -3,18 +3,33 @@ package com.example.backgom.linearlayoutexample;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+	@Bind(R.id.viewpager)
+	ViewPager pager;
+	@Bind(R.id.sliding_tabs)
+	TabLayout tabLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
+
+		pager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
+		tabLayout.setupWithViewPager(pager);
+
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
